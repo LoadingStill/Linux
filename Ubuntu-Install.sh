4ca5a6c74e55d3574c87a84359c985f6aafa5482
Ryan Young <15201984+LoadingStill@users.noreply.github.com>
2022-03-14 09:53:04 -0400

    Update Ubuntu-Install.sh
    changed libreoffice from flatpack to ppa install.

--- Ubuntu-Install.sh
+++ Ubuntu-Install.sh
@@ -2,5 +2,9 @@
 #update
 sudo apt update
 sudo apt full-upgrade -y
-#install libreoffice flatpack
-flatpak uninstall org.libreoffice.LibreOffice//fresh
+#install libreoffice repository
+sudo add-apt-repository ppa:libreoffice/ppa
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt install libreoffice
+#install 
