a69df25ac652ceaf84538759729d3c5db5810182
Ryan Young <15201984+LoadingStill@users.noreply.github.com>
2022-03-14 11:50:11 -0400

    ubuntu-install-update

--- Ubuntu-Install.sh
+++ Ubuntu-Install.sh
@@ -1,10 +1,17 @@
 !/bin/bash
-#update
+#update system
 sudo apt update
 sudo apt full-upgrade -y
+#make sure flatpack is installed
+sudo apt install flatpakv
+flatpack update
 #install libreoffice repository
 sudo add-apt-repository ppa:libreoffice/ppa
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 sudo apt install libreoffice
-#install 
+#install GIMP flatpack
+#update flatpack
+sudo flatpak update
+sudo flatpak install https://flathub.org/repo/appstream/org.gimp.GIMP.flatpakref
+#if program will not run by clicking shortcut run | flatpak run org.gimp.GIMP//stable
