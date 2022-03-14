1f983c3849b40ef5a33409fda88f392e2cc7a2ae
Ryan Young <15201984+LoadingStill@users.noreply.github.com>
2022-03-14 11:27:33 -0400

    Update Ubuntu-Install.sh
    added -y to line 8

--- Ubuntu-Install.sh
+++ Ubuntu-Install.sh
@@ -5,6 +5,6 @@ sudo apt full-upgrade -y
 #install libreoffice repository
 sudo add-apt-repository ppa:libreoffice/ppa
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 sudo apt install libreoffice
 #install 
