6b0b4e889ad55a4504053b5fefdc8205945f409b
Ryan Young <15201984+LoadingStill@users.noreply.github.com>
2022-03-16 09:48:19 -0400

    Create install-obs.sh
    added obs code

---
+++ software/debian/install-obs.sh
@@ -0,0 +1,5 @@
+echo "########################  obs installing  #######################"
+sudo add-apt-repository ppa:obsproject/obs-studio -y
+sudo apt update
+sudo apt install -y obs-studio ffmpeg
+echo "########################  obs installed  #######################"
