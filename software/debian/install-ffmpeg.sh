b45cab0c01d8613ee31cf0a66ca854b5b0a76ce1
Ryan Young <15201984+LoadingStill@users.noreply.github.com>
2022-03-16 10:39:39 -0400

    Update install-ffmpeg.sh

--- software/debian/install-ffmpeg.sh
+++ software/debian/install-ffmpeg.sh
@@ -1,5 +1,6 @@
 echo "########################  ffmpeg installing  #######################"
 sudo apt-get remove ffmpeg
+sudo apt-get remove ffmpeg-real
 sudo apt-add-repository ppa:samrog131/ppa
 sudo apt-get update
 sudo apt-get install ffmpeg-real
