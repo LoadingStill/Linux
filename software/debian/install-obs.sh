ed25581824aee94d0d0c2fc02c255c5b794ec6c6
Ryan Young <15201984+LoadingStill@users.noreply.github.com>
2022-03-16 09:58:57 -0400

    Update install-obs.sh

--- software/debian/install-obs.sh
+++ software/debian/install-obs.sh
@@ -1,5 +1,7 @@
 echo "########################  obs installing  #######################"
+
+sudo apt install v4l2loopback-dkms
 sudo add-apt-repository ppa:obsproject/obs-studio -y
 sudo apt update
-sudo apt install -y obs-studio ffmpeg
+sudo apt install -y obs-studio
 echo "########################  obs installed  #######################"
