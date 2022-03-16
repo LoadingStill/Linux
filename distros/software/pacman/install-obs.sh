5f438f2f9454eb09de406d6dee9aa56f36290f2e
Ryan Young <15201984+LoadingStill@users.noreply.github.com>
2022-03-16 14:04:01 -0400

    Update install-obs.sh

--- distros/software/pacman/install-obs.sh
+++ distros/software/pacman/install-obs.sh
@@ -1,1 +1,7 @@
 #!/bin/bash
+echo "########################  obs installing  #######################"
+sudo pacman -S obs-studio-tytan652
+sudo pacman -S obs-studio-browser
+sudo pacman -S obs-studio-rc
+sudo pacman -S obs-studio
+echo "########################  obs installed  #######################"
