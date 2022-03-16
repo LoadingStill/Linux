47719242c6187a1d37b2f775c4e5c54334d890a9
Ryan Young <15201984+LoadingStill@users.noreply.github.com>
2022-03-16 09:59:47 -0400

    Update install-obs.sh

--- software/debian/install-obs.sh
+++ software/debian/install-obs.sh
@@ -1,5 +1,6 @@
 echo "########################  obs installing  #######################"
-
+sudo apt-get remove obs
+sudo apt-get remove obs-studio
 sudo apt install v4l2loopback-dkms
 sudo add-apt-repository ppa:obsproject/obs-studio -y
 sudo apt update
