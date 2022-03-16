c0e73171e86b78d73763407a2332d7c191c4fb8a
Ryan Young <15201984+LoadingStill@users.noreply.github.com>
2022-03-16 14:16:15 -0400

    Update manjaro-all.sh

--- distros/manjaro-all.sh
+++ distros/manjaro-all.sh
@@ -1,3 +1,41 @@
 #!/bin/bash
-#update
+
+
+
+echo "################################################################"
+echo "#                                                              #"
+echo "#                   Manjaro Install Scrip                      #"
+echo "#                                                              #"
+echo "################################################################"
+
+
+
+echo "####################    Updating    #######################"
+#update and install
 sudo pacman -Syyu
+#Remove packages no longer included in any repositories.
+pacman -Qmq | pacman -Rs -
+echo "####################    Updated     #######################"
+
+
+
+echo "####################    Installing Software   #######################"
+sh software/pacman/install-obs.sh
+echo "####################    Software Installed    #######################"
+
+
+
+echo "####################    After Software Updates    #######################"
+#update and install
+sudo pacman -Syyu
+#Remove packages no longer included in any repositories.
+pacman -Qmq | pacman -Rs -
+echo "####################    Completed    #######################"
+
+
+
+echo "################################################################"
+echo "#                                                              #"
+echo "#                   Manjaro Install Script                     #"
+echo "#                                                              #"
+echo "################################################################"
