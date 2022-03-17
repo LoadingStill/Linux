1269d0e22a326a1d86bbae444f793a0c67e58b89
Ryan Young <15201984+LoadingStill@users.noreply.github.com>
2022-03-17 12:00:58 -0400

    update install.sh

--- distros/software/apt/install-vlc.sh
+++ distros/software/apt/install-vlc.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "########################  git installing  #######################"
-sudo apt-get remove git
+sudo apt-get remove vlc
+sudo add-apt-repository ppa:videolan/master-daily
 sudo apt update
-sudo apt install -y git
+sudo apt install -y vlc
 echo "########################  git installed  #######################"
