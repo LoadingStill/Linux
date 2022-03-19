f1147cd70acdf62804a67d59b75a23d34df2bd36
Ryan Young <15201984+LoadingStill@users.noreply.github.com>
2022-03-18 23:01:43 -0400

    auto remove update

--- distros/ubuntu-all.sh
+++ distros/ubuntu-all.sh
@@ -14,7 +14,7 @@ echo "####################    Updating    #######################"
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
-sudo apt-get autoremove
+sudo apt-get autoremove -y
 echo "####################    Updated    #######################"
 
 
