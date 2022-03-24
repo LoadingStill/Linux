348095570f5e0a99bb5eb32c6cdc0bbad6b2c1a0
Ryan Young <youngrya001@gmail.com>
2022-03-24 19:04:07 -0400

    Update install-flatpack.sh

--- distros/software/dnf/install-flatpack.sh
+++ distros/software/dnf/install-flatpack.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-
+echo "########################  flatpack installing  #######################"
 # Installing a package
 flatpak install fedora $APPLICATION
 # Removing a package
 flatpak remove $APPLICATION
 # Updating packages
 flatpak update
+echo "########################  flatpack  installed  #######################"
