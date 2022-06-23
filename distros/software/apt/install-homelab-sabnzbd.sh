c1002fb7beeb88ba2e14c0814a125d66c752b2aa
Ryan Young <youngrya001@gmail.com>
2022-06-23 14:49:43 -0400

    Update install-homelab-sabnzbd.sh

--- distros/software/apt/install-homelab-sabnzbd.sh
+++ distros/software/apt/install-homelab-sabnzbd.sh
@@ -1,1 +1,5 @@
 !#/bin/bash
+echo "####################    SABnzbd Installing  #######################"
+sudo apt-get install sabnzbdplus
+sabnzbdplus
+echo "####################    SABnzbd Installed   #######################"
