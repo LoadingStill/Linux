a0f75da33f08cabdad31e8e443acc75f4be34c02
Ryan Young <youngrya001@gmail.com>
2022-08-31 10:59:29 -0400

    Update fedora-all.sh
    added reboot

--- distros/fedora-all.sh
+++ distros/fedora-all.sh
@@ -37,6 +37,23 @@ echo "####################    Completed    #######################"
 eecho "################################################################"
 echo "#                                                              #"
 echo "#                    Completed fedora-all.sh                   #"
-echo "#                  Please restart your system!                 #"
 echo "#                                                              #"
 echo "################################################################"
+
+
+
+echo "####################    Rebooting    #######################"
+echo "Rebooting in 5     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 4     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 3     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 2     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 1     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 0     Press Ctrl + C to cancel"
+echo "####################    Rebooting    #######################"
+
+sudo reboot now
