4575aca5371ac48734bf14448ca81d3ab3558df6
Ryan Young <youngrya001@gmail.com>
2022-08-31 10:58:17 -0400

    Update debian-all.sh
    added rebooting

--- distros/debian-all.sh
+++ distros/debian-all.sh
@@ -83,3 +83,19 @@ echo "#                                                              #"
 echo "#                    Completed Debian.sh                       #"
 echo "#                                                              #"
 echo "################################################################"
+
+
+
+echo "####################    Rebooting    #######################"
+echo "Rebooting in 5     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 4     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 3     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 2     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 1     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 0     Press Ctrl + C to cancel"
+echo "####################    Rebooting    #######################"
