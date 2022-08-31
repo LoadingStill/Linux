0195cd7f3cf29b95c14f0a935f2b8e945b8b97b7
Ryan Young <youngrya001@gmail.com>
2022-08-31 10:58:39 -0400

    Update debian-all.sh

--- distros/debian-all.sh
+++ distros/debian-all.sh
@@ -99,3 +99,5 @@ echo "Rebooting in 1     Press Ctrl + C to cancel"
 sleep 1
 echo "Rebooting in 0     Press Ctrl + C to cancel"
 echo "####################    Rebooting    #######################"
+
+sudo reboot now
