aaeb3ae7747fc6be60850a20d2e9528d173e9a85
Ryan Young <youngrya001@gmail.com>
2022-08-31 08:32:53 -0400

    Update install-desktop-discord.sh
    echo "THIS SOFTWARE IS PROPRIETARY"

--- distros/software/apt/install-desktop-discord.sh
+++ distros/software/apt/install-desktop-discord.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "########################  discord installing  #######################"
+echo "THIS SOFTWARE IS PROPRIETARY"
+echo "THIS SOFTWARE IS PROPRIETARY"
+echo "THIS SOFTWARE IS PROPRIETARY"
 wget -O discord.deb "https://discordapp.com/api/download?platform=linux&format=deb"
 sudo dpkg -i /path/to/discord.deb
 echo "########################  discord  installed  #######################"
