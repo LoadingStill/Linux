51b6a9956a820b344143a41ad47d0b70651dc747
Ryan Young <youngrya001@gmail.com>
2022-08-31 11:00:57 -0400

    Update ubuntu-all.sh
    added reboot now

--- distros/ubuntu-all.sh
+++ distros/ubuntu-all.sh
@@ -87,3 +87,21 @@ echo "#                    Completed ubuntu-all.sh                   #"
 echo "#                 Please restart your copmputer!               #"
 echo "#                                                              #"
 echo "################################################################"
+
+
+
+echo "####################    Rebooting    #######################"
+echo "Rebooting in 5     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 4     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 3     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 2     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 1     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 0     Press Ctrl + C to cancel"
+echo "####################    Rebooting    #######################"
+
+sudo reboot now
