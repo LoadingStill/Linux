2ee5a89313a6bff4bbab6425f29a466b65db66ac
Ryan Young <youngrya001@gmail.com>
2022-08-31 10:58:55 -0400

    Update debian-homelab.sh
    added reboot

--- distros/debian-homelab.sh
+++ distros/debian-homelab.sh
@@ -40,3 +40,21 @@ echo "#                                                                      #"
 echo "#                    Completed Debian Homelab.sh                       #"
 echo "#                                                                      #"
 echo "########################################################################"
+
+
+
+echo "####################    Rebooting    #######################"
+echo "Rebooting in 5     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 4     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 3     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 2     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 1     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 0     Press Ctrl + C to cancel"
+echo "####################    Rebooting    #######################"
+
+sudo reboot now
