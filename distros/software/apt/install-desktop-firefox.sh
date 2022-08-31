eeda987a183eceba27ea507dad3448e964bea61b
Ryan Young <youngrya001@gmail.com>
2022-08-31 08:23:27 -0400

    Update install-desktop-firefox.sh

--- distros/software/apt/install-desktop-firefox.sh
+++ distros/software/apt/install-desktop-firefox.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+echo "########################  firefox installing  #######################"
 sudo apt update
 sudo snap remove firefox -y
 sudo apt install firefox -y
+echo "########################  firefox  installed  #######################"
