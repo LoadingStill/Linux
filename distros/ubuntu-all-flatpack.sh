687425174d54956d2b7761caa2cdb55cf9984d0f
Ryan Young <youngrya001@gmail.com>
2022-08-31 11:00:39 -0400

    Update ubuntu-all-flatpack.sh
    added reboot now

--- distros/ubuntu-all-flatpack.sh
+++ distros/ubuntu-all-flatpack.sh
@@ -85,6 +85,23 @@ echo "####################  Completed Software Install  #######################"
 echo "################################################################"
 echo "#                                                              #"
 echo "#                    Completed ubuntu-all.sh                   #"
-echo "#                 Please restart your copmputer!               #"
 echo "#                                                              #"
 echo "################################################################"
+
+
+
+echo "####################    Rebooting    #######################"
+echo "Rebooting in 5     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 4     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 3     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 2     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 1     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 0     Press Ctrl + C to cancel"
+echo "####################    Rebooting    #######################"
+
+sudo reboot now
