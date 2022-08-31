4bcdf3fad34eccd471e8cf047e559667ff856e19
Ryan Young <youngrya001@gmail.com>
2022-08-31 11:00:18 -0400

    Update redhat-all.sh
    added reboot now

--- distros/redhat-all.sh
+++ distros/redhat-all.sh
@@ -1,1 +1,23 @@
 #!/bin/bash
+
+
+
+
+
+
+
+echo "####################    Rebooting    #######################"
+echo "Rebooting in 5     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 4     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 3     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 2     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 1     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 0     Press Ctrl + C to cancel"
+echo "####################    Rebooting    #######################"
+
+sudo reboot now
