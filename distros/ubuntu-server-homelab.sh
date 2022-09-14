89d88cc1068271e7838bd32876736e6070adb6ac
Ryan Young <youngrya001@gmail.com>
2022-09-14 11:24:09 -0400

    Update ubuntu-server-homelab.sh
    added doc for logs and to show what ports to use

--- distros/ubuntu-server-homelab.sh
+++ distros/ubuntu-server-homelab.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+#create file with info on what was done saved to /home/$USERNAME/servarrInfo
+touch servarrInfo
 
+#Vatiables
+ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1) #IPv4 address
+#ip6=$(/sbin/ip -o -6 addr list eth0 | awk '{print $4}' | cut -d/ -f1) #IPv6 address
 
 echo "###############################################################################"
 echo "#                                                                             #"
@@ -20,12 +25,31 @@ echo "####################    Updated    #######################"
 
 echo "####################    Installing Software  #######################"
 sh software/apt/install-homelab-jellyfin.sh #installs jellyfin
+echo 'jellyfin installed - LOCALIP:8096' >> /home/$USERNAME/servarrInfo
+
 sh software/apt/install-homelab-servarr-sonarr.sh #installs sonarr
+echo 'sonarr installed - LOCALIP:8989' >> /home/$USERNAME/servarrInfo
+
 sh software/apt/install-homelab-servarr-radarr.sh #installs radarr
+echo 'radarr installed - LOCALIP:7878' >> /home/$USERNAME/servarrInfo
+
 sh software/apt/install-homelab-servarr-readarr.sh #installs readarr
+echo 'readarr installed - LOCALIP:8787' >> /home/$USERNAME/servarrInfo
+
 sh software/apt/install-homelab-servarr-prowlarr.sh #installs prowlarr
+echo 'prowlarr installed - LOCALIP:9696' >> /home/$USERNAME/servarrInfo
+
 sh software/apt/install-homelab-sabnzbd.sh #installs prowlarr
+echo 'sabnzbd installed - LOCALIP:8080' >> /home/$USERNAME/servarrInfo
 echo "####################    Software Installed   #######################"
+
+
+echo "####################    Setting Up fstab     ####################"
+sudo mkdir /mnt/media
+sudo apt install nfs-common -y
+sudo echo '10.1.1.26:/mnt/servarr/media  /mnt/media  nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0' >> /etc/fstab
+echo "####################  fstab setup completed  ####################"
+
 
 
 echo "####################    Updating    #######################"
@@ -33,6 +57,9 @@ sudo apt update
 sudo apt upgrade -y
 sudo apt autoremove -y
 echo "####################    Updated    ########################"
+
+
+
 
 
 
