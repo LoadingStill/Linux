d431c4e3dd3b9d010b7d3d7a80fefce258937108
Ryan Young <youngrya001@gmail.com>
2022-09-14 10:43:56 -0400

    Update ubuntu-server-homelab.sh

--- distros/ubuntu-server-homelab.sh
+++ distros/ubuntu-server-homelab.sh
@@ -1,1 +1,61 @@
 #!/bin/bash
+
+
+
+echo "###############################################################################"
+echo "#                                                                             #"
+echo "#                    Ubuntu Server Homelab Install Scrip                      #"
+echo "#                                                                             #"
+echo "###############################################################################"
+
+
+
+echo "####################    Updating    #######################"
+sudo apt update -y
+sudo apt upgrade -y
+#sudo apt dist-upgrade
+echo "####################    Updated    #######################"
+
+
+
+echo "####################    Installing Software  #######################"
+sh software/apt/install-homelab-jellyfin.sh #installs jellyfin
+sh software/apt/install-homelab-servarr-sonarr.sh #installs sonarr
+sh software/apt/install-homelab-servarr-radarr.sh #installs radarr
+sh software/apt/install-homelab-servarr-readarr.sh #installs readarr
+sh software/apt/install-homelab-servarr-prowlarr.sh #installs prowlarr
+sh software/apt/install-homelab-sabnzbd.sh #installs prowlarr
+echo "####################    Software Installed   #######################"
+
+
+echo "####################    Updating    #######################"
+sudo apt update
+sudo apt upgrade -y
+sudo apt autoremove -y
+echo "####################    Updated    ########################"
+
+
+
+echo "#########################################################################################"
+echo "#                                                                                       #"
+echo "#                    Completed Ubuntu Server Homelab Install Scrip                      #"
+echo "#                                                                                       #"
+echo "#########################################################################################"
+
+
+
+echo "####################    Rebooting    #######################"
+echo "Rebooting in 5     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 4     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 3     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 2     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 1     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 0     Press Ctrl + C to cancel"
+echo "####################    Rebooting    #######################"
+
+sudo reboot now
