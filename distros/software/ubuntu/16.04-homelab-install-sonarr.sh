69b681d55a19b93a52526137a8a45909b2f83410
Ryan Young <youngrya001@gmail.com>
2022-09-15 08:17:55 -0400

    Update 16.04-homelab-install-sonarr.sh

--- distros/software/ubuntu/16.04-homelab-install-sonarr.sh
+++ distros/software/ubuntu/16.04-homelab-install-sonarr.sh
@@ -3,4 +3,7 @@ echo "########################  Sonarr Installing  #######################"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 2009837CBFFD68F45BC180471F4F90DE2A9B4BF8
 echo "deb https://apt.sonarr.tv/ubuntu xenial main" | sudo tee /etc/apt/sources.list.d/sonarr.list
 sudo apt update
+sudo apt install sonarr
+sudo systemctl enable sonarr
+sudo systemctl start sonarr
 echo "########################  Sonarr Installed  #######################"
