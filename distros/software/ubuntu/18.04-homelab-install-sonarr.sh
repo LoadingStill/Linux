e29820269e1224a66231d4189e4163e8318ff2fb
Ryan Young <youngrya001@gmail.com>
2022-09-15 08:17:37 -0400

    Update 18.04-homelab-install-sonarr.sh

--- distros/software/ubuntu/18.04-homelab-install-sonarr.sh
+++ distros/software/ubuntu/18.04-homelab-install-sonarr.sh
@@ -3,4 +3,6 @@ echo "########################  Sonarr Installing  #######################"
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 2009837CBFFD68F45BC180471F4F90DE2A9B4BF8
 echo "deb https://apt.sonarr.tv/ubuntu bionic main" | sudo tee /etc/apt/sources.list.d/sonarr.list
 sudo apt update
+sudo systemctl enable sonarr
+sudo systemctl start sonarr
 echo "########################  Sonarr Installed  #######################"
