495bc4f23e91ab971ca17c9031bf536d354cceb0
Ryan Young <youngrya001@gmail.com>
2022-09-15 08:11:37 -0400

    Update 20.04-homelab-install-sabnzbd.sh

--- distros/software/ubuntu/20.04-homelab-install-sabnzbd.sh
+++ distros/software/ubuntu/20.04-homelab-install-sabnzbd.sh
@@ -7,13 +7,8 @@ sudo add-apt-repository ppa:jcfp/nobetas  #stable
 #sudo add-apt-repository ppa:jcfp/ppa     #alpha/beta/rc
 sudo apt-get update && sudo apt-get dist-upgrade
 sudo apt-get install sabnzbdplus
-
-#
-# edit /etc/default/sabnzbdplus
-# USER=whoami
-#
-
+sed '/USER/s/$/ $USERNAME/' /etc/default/sabnabdplus  #addes username to the end of the line where username is needed to start program in background on boot
 sudo systemctl daemon-reload
-
 sudo service sabnzbdplus start  #starts sabnzbd at boot
 #sudo service sabnzbdplus stop  ##starts sabnzbd at boot
+echo "####################    SABnzbd  Installed  #######################"
