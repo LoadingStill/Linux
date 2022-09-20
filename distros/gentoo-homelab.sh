8ecf0794496807681d85318cdd432838d572e08b
Ryan Young <youngrya001@gmail.com>
2022-09-20 12:13:31 -0400

    Create gentoo-homelab.sh

---
+++ distros/gentoo-homelab.sh
@@ -0,0 +1,11 @@
+#Current Project... Will take some time new to Gentoo
+
+#!/bin/bash
+
+
+
+echo "################################################################"
+echo "#                                                              #"
+echo "#                    Gentoo Install Scrip                      #"
+echo "#                                                              #"
+echo "################################################################"
