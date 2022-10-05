d29483c007e468b06fa65b9bf89eb2d083ec16aa
Ryan Young <youngrya001@gmail.com>
2022-10-05 10:19:00 -0400

    Create system-update.sh

---
+++ distros/software/dnf/system-update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+echo "####################    System Upgrading    #######################"
+sudo dnf check-update
+sudo dnf update -y
+sudo dnf autoremove
+#sudo dnf distro-sync (if things breake run this line by it self)
+echo "####################    System Upgraded    #######################"
+
+
+
+echo "####################    Rebooting    #######################"
+echo "Rebooting in 5     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 4     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 3     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 2     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 1     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 0     Press Ctrl + C to cancel"
+echo "####################    Rebooting    #######################"
+sudo reboot now
