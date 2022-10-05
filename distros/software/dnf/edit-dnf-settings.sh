73547c74f8f373122d8d2c9beb3663dbd7ed18a2
Ryan Young <youngrya001@gmail.com>
2022-10-05 09:19:03 -0400

    Create edit-dnf-settings.sh

---
+++ distros/software/dnf/edit-dnf-settings.sh
@@ -0,0 +1,3 @@
+echo "####################    dnf max parallel downloads    #######################"
+echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf #changes dnf from single package update/install at once to 10 at once (change to lower number for older systems)
+echo "####################    dnf max parallel downloads    #######################"
