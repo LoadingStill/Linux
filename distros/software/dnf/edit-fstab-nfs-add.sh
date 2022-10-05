835a85e7a351aaa03091a34c667d1d3dda45ea0f
Ryan Young <youngrya001@gmail.com>
2022-10-05 10:43:18 -0400

    Create edit-fstab-nfs-add.sh

---
+++ distros/software/dnf/edit-fstab-nfs-add.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#YOU NEED TO EDIT THIS FILE FOR THIS TO WORK
+
+
+
+echo "####################    setting Up fstab     ####################"
+sudo mkdir /mnt/FileNameHere
+dnf -y install nfs-utils
+echo 'nfs-utils' >> /home/$USERNAME/servarrInfo
+sudo echo 'nfsIP:/mount/directory/folder  /mnt/FileNameHere  nfs defaults 0 0' >> /etc/fstab
+echo "####################  fstab setup completed  ####################"
