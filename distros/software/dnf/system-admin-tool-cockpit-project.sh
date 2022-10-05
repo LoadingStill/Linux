caeafd388c5e05bce6973a4cb70915b34de3bebc
Ryan Young <youngrya001@gmail.com>
2022-10-05 13:15:11 -0400

    Update system-admin-tool-cockpit-project.sh

--- distros/software/dnf/system-admin-tool-cockpit-project.sh
+++ distros/software/dnf/system-admin-tool-cockpit-project.sh
@@ -12,7 +12,15 @@ sudo dnf install -y cockpit-sosreport
 sudo dnf install -y cockpit-composer
 sudo dnf install -y cockpit-389-ds
 sudo dnf install -y cockpit-session-recording
-#subscription-manager-cockpit
-#cockpit-ovirt-dashboard
-#cockpit-sensors
+sudo dnf install -y cockpit-sensors #List all available sensors on the machine provided by lm-sensors
+sudo dnf install -y cockpit-zfs #An interactive ZFS on Linux admin package for Cockpit.
+#sudo dnf install -y subscription-manager-cockpit #Manage subscriptions to Red Hat Enterprise Linux from your web browser.
+#sudo dnf install -y cockpit-ovirt-dashboard #oVirt manages Virtual Machines (VMs) in a datacenter/cluster. Scales easily from tens to tens of thousands VMs running on multiple KVM hypervisor hosts.
 echo "####################    Cockpit Project Installed    #######################"
+
+
+
+
+
+
+#https://cockpit-project.org/applications.html
