5b4bdd9911d38f379abd359f7c71541556f9c88f
Ryan Young <youngrya001@gmail.com>
2022-10-05 11:15:33 -0400

    Update system-admin-tool-cockpit-project.sh

--- distros/software/dnf/system-admin-tool-cockpit-project.sh
+++ distros/software/dnf/system-admin-tool-cockpit-project.sh
@@ -4,4 +4,13 @@ sudo dnf install cockpit
 sudo systemctl enable --now cockpit.socket
 sudo firewall-cmd --add-service=cockpit
 sudo firewall-cmd --add-service=cockpit --permanent
+sudo dnf install cockpit-machines
+sudo dnf install cockpit-podman
+sudo dnf install cockpit-kdump
+sudo dnf install cockpit-certificates
+sudo dnf install cockpit-sosreport
+sudo dnf install cockpit-composer
+sudo dnf install cockpit-389-ds
+sudo dnf install cockpit-session-recording
+
 echo "####################    Cockpit Project Installed    #######################"
