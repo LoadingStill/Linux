b49b2bc2c198f6de1b51180e00582378f4707908
Ryan Young <youngrya001@gmail.com>
2022-10-05 10:13:10 -0400

    Update repository-rpm-free.sh

--- distros/software/dnf/repository-rpm-free.sh
+++ distros/software/dnf/repository-rpm-free.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "####################    RMP Fusion Free Installing    #######################"
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm  
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf update -y
 echo "####################    RMP Fusion Free Installed     #######################"
