46c990976313dce9bdbc18474b89160426338714
Ryan Young <youngrya001@gmail.com>
2022-10-05 10:07:09 -0400

    Update driver-nvidia-quadro-p2000.sh

--- distros/software/dnf/driver-nvidia-quadro-p2000.sh
+++ distros/software/dnf/driver-nvidia-quadro-p2000.sh
@@ -4,3 +4,8 @@ sudo dnf update -y # and reboot if you are not on the latest kernel
 sudo dnf install akmod-nvidia # rhel/centos users can use kmod-nvidia instead
 sudo dnf install xorg-x11-drv-nvidia-cuda #optional for cuda/nvdec/nvenc support
 echo "####################    Nvidia Driver Quadro P2000 Installed     #######################"
+
+
+
+#source
+#https://rpmfusion.org/Howto/NVIDIA
