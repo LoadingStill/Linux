7388b2922c0a3a4f013aa460d85fc6738232c64a
Ryan Young <youngrya001@gmail.com>
2022-10-05 11:26:08 -0400

    Update system-admin-tool-cockpit-project.sh

--- distros/software/dnf/system-admin-tool-cockpit-project.sh
+++ distros/software/dnf/system-admin-tool-cockpit-project.sh
@@ -12,5 +12,7 @@ sudo dnf install -y cockpit-sosreport
 sudo dnf install -y cockpit-composer
 sudo dnf install -y cockpit-389-ds
 sudo dnf install -y cockpit-session-recording
-
+#subscription-manager-cockpit
+#cockpit-ovirt-dashboard
+#cockpit-sensors
 echo "####################    Cockpit Project Installed    #######################"
