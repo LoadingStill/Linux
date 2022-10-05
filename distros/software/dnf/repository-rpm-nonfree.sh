a0ceb2c9d5b41edccf7c7925d353dc777a47dee3
Ryan Young <youngrya001@gmail.com>
2022-10-05 09:48:00 -0400

    Update repository-rpm-nonfree.sh

--- distros/software/dnf/repository-rpm-nonfree.sh
+++ distros/software/dnf/repository-rpm-nonfree.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "####################    RMP Fusion Free Installing    #######################"
-https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 echo "####################    RMP Fusion Free Installed     #######################"
