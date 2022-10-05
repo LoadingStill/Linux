0bd37a4fd0d0b207a100fa0aa9e9d9a43c7bf719
Ryan Young <youngrya001@gmail.com>
2022-10-05 09:19:58 -0400

    Update edit-dnf-settings.sh

--- distros/software/dnf/edit-dnf-settings.sh
+++ distros/software/dnf/edit-dnf-settings.sh
@@ -1,3 +1,4 @@
 echo "####################    dnf max parallel downloads    #######################"
 echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf #changes dnf from single package update/install at once to 10 at once (change to lower number for older systems)
+echo 'fastestmirror=True' | sudo tee -a /etc/dnf/dnf.conf #sets dnf to use the fastest mirror
 echo "####################    dnf max parallel downloads    #######################"
