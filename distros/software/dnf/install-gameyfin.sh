2740f2cf4ef840c3a99c128f87a8f96e1c9fbf97
Ryan Young <youngrya001@gmail.com>
2022-10-06 09:26:04 -0400

    Update install-gameyfin.sh

--- distros/software/dnf/install-gameyfin.sh
+++ distros/software/dnf/install-gameyfin.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 
 
+
 echo "####################    Gameyfin Installing    #######################"
 #you need java version 18+ and you need to download the .jar release from gameyfin github (2files .jar and .properties)
 #https://github.com/grimsi/gameyfin
 #https://api-docs.igdb.com/#about
 
 
+
 sudo dnf install -y java-latest-openjdk.x86_64
-mkdir /home/$(whoami)/servarrInfo/install_script/
-mkdir /home/$(whoami)/servarrInfo/install_script/gameyfin
-cd install_script/gameyfin/
-wget https://github.com/grimsi/gameyfin/releases/download/v1.2.1/gameyfin-1.2.1.jar
-wget https://github.com/grimsi/gameyfin/releases/download/v1.2.1/gameyfin.properties
-cd
+wget https://github.com/grimsi/gameyfin/releases/download/v1.2.1/gameyfin-1.2.1.jar -P /home/$(whoami)/servarrInfo/install_script/gameyfin/
+wget https://github.com/grimsi/gameyfin/releases/download/v1.2.1/gameyfin.properties -P /home/$(whoami)/servarrInfo/install_script/gameyfin/
 
 
-echo 'gameyfin doenloaded, you NEED to edit the properties file before you can run this program. location /home/$(whoami)/servarrInfo/install_script/gameyfin' >> /home/$(whoami)/servarrInfo
+
+echo 'gameyfin doenloaded, you NEED to edit the properties file before you can run this program. location /home/$(whoami)/servarrInfo/install_script/gameyfin' >> /home/$(whoami)/servarrInfo/log
 
 
 
