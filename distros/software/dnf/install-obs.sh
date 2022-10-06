5152d0037b358a072e71b42bf05f9cd1cca8b466
Ryan Young <youngrya001@gmail.com>
2022-10-06 10:35:04 -0400

    Update install-obs.sh

--- distros/software/dnf/install-obs.sh
+++ distros/software/dnf/install-obs.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 echo "########################  obs installing  #######################"
-
 sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-sudo dnf install obs-studio
-#uncomment line 7 if you use a nvidia gpu! (delete the #)
-#sudo dnf install xorg-x11-drv-nvidia-cuda
-
-#for older nvidia gpus use uncommit line 10 and not line 7 (delete the #)
-#sudo dnf install xorg-x11-drv-nvidia-340xx-cuda
-
+sudo dnf install -y kmod-v4l2loopback # virtual camera support
+sudo dnf install -y obs-studio
+#sudo dnf install xorg-x11-drv-nvidia-cuda #uncomment this line if you use a nvidia gpu! (delete the 1st # only) newer nvidia gpu
+#sudo dnf install xorg-x11-drv-nvidia-340xx-cuda #for older nvidia gpus uncommit this line and NOT the line above  (delete the 1st #)
 echo "########################  obs installed  #######################"
