9a9e6c09b0f5d74b5133d2bce02b1eb15a3fa3eb
Ryan Young <youngrya001@gmail.com>
2022-10-06 09:59:37 -0400

    Update install-flatpack.sh

--- distros/software/dnf/install-flatpack.sh
+++ distros/software/dnf/install-flatpack.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "########################  flatpack installing  #######################"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+echo 'flatpack repo added' >> /home/$(whoami)/info/log #add info to log
 echo "########################  flatpack  installed  #######################"
