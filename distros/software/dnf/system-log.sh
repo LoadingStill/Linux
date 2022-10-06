dec787d5a41d5909047502661b64fa423819e367
Ryan Young <youngrya001@gmail.com>
2022-10-06 09:43:19 -0400

    Create system-log.sh

---
+++ distros/software/dnf/system-log.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "####################    create log & info    #######################"
+mkdir /home/$(whoami)/info
+touch /home/$(whoami)/info/log
+echo "####################    log file created     #######################"
