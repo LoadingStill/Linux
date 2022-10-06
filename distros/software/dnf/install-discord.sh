a97a1fbc4d3507ea0bf2e04cf19246c7d21c1082
Ryan Young <youngrya001@gmail.com>
2022-10-06 09:55:52 -0400

    Update install-discord.sh

--- distros/software/dnf/install-discord.sh
+++ distros/software/dnf/install-discord.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 echo "########################  obs installing  #######################"
-#sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-#sudo dnf update
-#sudo dnf install discord
-
-
+sudo dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf update
+sudo dnf install -y discord
 #curl -o ~/Downloads/ https://discordapp.com/api/download?platform=linux&format=tar.gz
+echo 'discord downloaded' >> /home/$(whoami)/info/log #add info to log
 echo "########################  obs installed  #######################"
