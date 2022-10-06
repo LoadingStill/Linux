d5499c8896804a0f0f63f88a786e9f7142712f4c
Ryan Young <youngrya001@gmail.com>
2022-10-06 08:25:24 -0400

    Update ubuntu-server-homelab.sh
    changed $USERNAME to $(whoami)

--- distros/ubuntu-server-homelab.sh
+++ distros/ubuntu-server-homelab.sh
@@ -9,7 +9,7 @@ ip4=$(/sbin/ip -o -4 addr list eth0 | awk '{print $4}' | cut -d/ -f1) #IPv4 addr
 
 
 
-#create file with info on what was done saved to /home/$USERNAME/servarrInfo
+#create file with info on what was done saved to /home/$(whoami)/servarrInfo
 touch /home/$(whoami)/servarrInfo
 
 
@@ -30,22 +30,22 @@ echo "####################    Updated    #######################"
 
 echo "####################    Installing Software  #######################"
 sh software/apt/install-homelab-jellyfin.sh #installs jellyfin
-echo 'jellyfin installed - $ip4:8096' >> /home/$USERNAME/servarrInfo
+echo 'jellyfin installed - $ip4:8096' >> /home/$(whoami)/servarrInfo
 
 sh software/ubuntu/20.04-homelab-install-sonarr.sh #installs sonarr
-echo 'sonarr installed - $ip4:8989' >> /home/$USERNAME/servarrInfo
+echo 'sonarr installed - $ip4:8989' >> /home/$(whoami)/servarrInfo
 
 sh software/apt/install-homelab-servarr-radarr.sh #installs radarr
-echo 'radarr installed - $ip4:7878' >> /home/$USERNAME/servarrInfo
+echo 'radarr installed - $ip4:7878' >> /home/$(whoami)/servarrInfo
 
 sh software/apt/install-homelab-servarr-readarr.sh #installs readarr
-echo 'readarr installed - $ip4:8787' >> /home/$USERNAME/servarrInfo
+echo 'readarr installed - $ip4:8787' >> /home/$(whoami)/servarrInfo
 
 sh software/apt/install-homelab-servarr-prowlarr.sh #installs prowlarr
-echo 'prowlarr installed - $ip4:9696' >> /home/$USERNAME/servarrInfo
+echo 'prowlarr installed - $ip4:9696' >> /home/$(whoami)/servarrInfo
 
 sh software/ubuntu/20.04-homelab-install-sabnzbd.sh #installs sabnzbd
-echo 'sabnzbd installed - $ip4:8080' >> /home/$USERNAME/servarrInfo
+echo 'sabnzbd installed - $ip4:8080' >> /home/$(whoami)/servarrInfo
 echo "####################    Software Installed   #######################"
 
 
@@ -53,7 +53,7 @@ echo "####################    Software Installed   #######################"
 #echo "####################    setting Up fstab     ####################"
 #sudo mkdir /mnt/media
 #sudo apt install nfs-common -y
-#echo 'nfs-commons installed' >> /home/$USERNAME/servarrInfo
+#echo 'nfs-commons installed' >> /home/$(whoami)/servarrInfo
 #sudo echo 'nfsIP:/mount/directory/folder  /mnt/media  nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0' >> /etc/fstab
 #echo "####################  fstab setup completed  ####################"
 
