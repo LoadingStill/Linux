d483c8564b99d00634824245eab12da9ed0dc977
Ryan Young <youngrya001@gmail.com>
2022-10-06 09:52:52 -0400

    Update driver-nvidia-quadro-p2000.sh

--- distros/software/dnf/driver-nvidia-quadro-p2000.sh
+++ distros/software/dnf/driver-nvidia-quadro-p2000.sh
@@ -3,7 +3,7 @@ echo "####################    Nvidia Quadro P2000 Driver Installing    #########
 sudo dnf update -y # and reboot if you are not on the latest kernel
 sudo dnf install akmod-nvidia # rhel/centos users can use kmod-nvidia instead
 sudo dnf install xorg-x11-drv-nvidia-cuda #optional for cuda/nvdec/nvenc support
-echo 'Nvidia Driver Quadro P2000 Installed - 510+ driver' >> /home/$(whoami)/info
+echo 'Nvidia Driver Quadro P2000 Installed - 510+ driver' >> /home/$(whoami)/info/log
 echo "####################    Nvidia Driver Quadro P2000 Installed     #######################"
 
 
