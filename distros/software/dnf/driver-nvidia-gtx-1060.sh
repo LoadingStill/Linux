87bc9793bda45c38438d92708102254d072f1a20
Ryan Young <youngrya001@gmail.com>
2022-10-14 10:00:19 -0400

    Update driver-nvidia-gtx-1060.sh

--- distros/software/dnf/driver-nvidia-gtx-1060.sh
+++ distros/software/dnf/driver-nvidia-gtx-1060.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo "####################    Nvidia GTX 1060 Driver Installing    #######################"
 sudo dnf update -y # and reboot if you are not on the latest kernel
-sudo dnf install akmod-nvidia # rhel/centos users can use kmod-nvidia instead
-sudo dnf install xorg-x11-drv-nvidia-cuda #optional for cuda/nvdec/nvenc support
+sudo dnf install -y akmod-nvidia # rhel/centos users can use kmod-nvidia instead
+sudo dnf install -y xorg-x11-drv-nvidia-cuda #optional for cuda/nvdec/nvenc support
 echo 'Nvidia Driver GTX 1060 Installed - 510+ driver' >> /home/$(whoami)/info/log
 echo "####################    Nvidia GTX 1060 P2000 Installed     #######################"
 
