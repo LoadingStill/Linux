7d2845aac70a2e95ba0f561dbf524af0f6499e63
Ryan Young <youngrya001@gmail.com>
2022-10-14 11:09:07 -0400

    Update edit-fstab-add-nfs.sh

--- distros/software/dnf/edit-fstab-add-nfs.sh
+++ distros/software/dnf/edit-fstab-add-nfs.sh
@@ -7,7 +7,7 @@
 
 echo "####################    setting Up fstab     ####################"
 sudo mkdir /mnt/FileNameHere
-dnf -y install nfs-utils
+sudo dnf -y install nfs-utils
 sudo echo 'nfsIP:/mount/directory/folder  /mnt/FileNameHere  nfs defaults 0 0' >> /etc/fstab
 
 echo 'nfs enabled' >> /home/$(whoami)/info/log #add info to log
