7aa478f356f1a25304b146c421ed0ef4e8290768
Ryan Young <youngrya001@gmail.com>
2022-10-17 08:47:08 -0400

    Update install-jellyfin.sh
    added links for reference

--- distros/software/dnf/install-jellyfin.sh
+++ distros/software/dnf/install-jellyfin.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
 echo "####################    Jellyfin Installing   #######################"
-#sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-#sudo dnf install https://repo.jellyfin.org/releases/server/fedora/stable/server/jellyfin-server-10.8.5-1.fc36.x86_64.rpm
-#sudo dnf install https://repo.jellyfin.org/releases/server/fedora/stable/web/jellyfin-web-10.8.5-1.fc36.noarch.rpm
-#sudo systemctl start jellyfin
-#sudo systemctl enable jellyfin
-#sudo firewall-cmd --permanent --add-service=jellyfin
-#sudo systemctl reboot
-
-
-
-mkdir /jellyifn
-wget https://repo.jellyfin.org/releases/server/fedora/stable/server/jellyfin-server-10.8.5-1.fc36.x86_64.rpm -p /jellyfin
-wget https://repo.jellyfin.org/releases/server/fedora/stable/web/jellyfin-web-10.8.5-1.fc36.noarch.rpm -p /jellyfin
-cd /jellyfin
-sudo rpm -i jellyfin-server-10.8.5-1.fc36.x86_64.rpm
-sudo rpm -i jellyfin-web-10.8.5-1.fc36.noarch.rpm
-cd
+#jellyfin offical instructions for installing jellyifn on fedora
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://repo.jellyfin.org/releases/server/fedora/stable/server/jellyfin-server-10.8.5-1.fc36.x86_64.rpm
+sudo dnf install https://repo.jellyfin.org/releases/server/fedora/stable/web/jellyfin-web-10.8.5-1.fc36.noarch.rpm
+sudo systemctl start jellyfin
+sudo systemctl enable jellyfin
+sudo firewall-cmd --permanent --add-service=jellyfin
+sudo systemctl reboot
 
 #start on boot
   #change 'youruser' to the user you want to run this program at boot with 
@@ -33,4 +24,7 @@ echo "####################    Jellyfin Installed    #######################"
 
 
 
-#jellyfin server and web versions see link: https://repo.jellyfin.org/releases/server/fedora/stable/
+#jellyfin server and web versions see
+  #link: https://repo.jellyfin.org/releases/server/fedora/stable/
+#jellyifn insatll documents
+  #https://jellyfin.org/docs/general/administration/installing.html#linux
