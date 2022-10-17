3e61d2bdf14422f292d7875f91b8f2ab8e4a9798
Ryan Young <youngrya001@gmail.com>
2022-10-17 14:24:36 -0400

    Update install-qbittorrent.sh

--- distros/software/dnf/install-qbittorrent.sh
+++ distros/software/dnf/install-qbittorrent.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+echo "####################    qBittorrent Installing    #######################"
 sudo dnf install -y qbittorrent
+echo "####################    qBittorrent Installed     #######################"
