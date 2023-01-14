f2b3bf5e31e319b07eca5c57a9c6d7a15142bf15
Ryan Young <youngrya001@gmail.com>
2023-01-14 16:39:01 +0000

    Update arch-desktop.sh

--- distros/arch-desktop.sh
+++ distros/arch-desktop.sh
@@ -4,7 +4,7 @@
 
 sudo pacman -Syy
 
-sudo pacman -S thunderbird kmymoney steam discord flatpak libreoffice-still firefox netfetch nano lutris wine winetricks packagekit-qt5 fwupd -y
+sudo pacman -S thunderbird kmymoney steam discord flatpak libreoffice-still firefox netfetch nano lutris wine winetricks packagekit-qt5 fwupd nfs-utils -y
 
 flatpak install flathub com.github.iwalton3.jellyfin-media-player   #jellyfin player
 flatpak install flathub org.kde.kdenlive                            #kdenlive
@@ -15,3 +15,22 @@ flatpak install flathub org.prismlauncher.PrismLauncher             #minecraft
 flatpak install flathub org.qbittorrent.qBittorrent                 #qbittorrent
 flatpak install flathub net.pcsx2.PCSX2                             #PCSX2
 
+touch /Desktop/update.sh
+sudo echo 'sudo pacman -Syu' >> /Desktop/update.sh
+sudo echo 'flatpak update -y' >> /Desktop/update.sh
+
+
+sudo mkdir /mnt/ryoung
+sudo mkdir /mnt/games
+sudo mkdir /mnt/proxmox
+sudo mkdir /mnt/homelab
+sudo mkdir /mnt/media
+sudo mkdir /mnt/HowToIT
+sudo mkdir /mnt/urbackup
+
+sudo echo '10.1.1.26:/mnt/servarr/ryoung  /mnt/ryoung  nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0' >> /etc/fstab
+sudo echo '10.1.1.26:/mnt/servarr/games  /mnt/games  nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0' >> /etc/fstab
+sudo echo '10.1.1.26:/mnt/servarr/proxmox  /mnt/proxmox  nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0' >> /etc/fstab
+sudo echo '10.1.1.26:/mnt/servarr/HowToIT  /mnt/HowToIT  nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0' >> /etc/fstab
+sudo echo '10.1.1.26:/mnt/servarr/media  /mnt/media  nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0' >> /etc/fstab
+sudo echo '10.1.1.26:/mnt/servarr/urbackup  /mnt/urbackup  nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0' >> /etc/fstab
