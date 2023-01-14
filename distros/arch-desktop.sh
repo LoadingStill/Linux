86f80bfcbd6c445a10613ebad6e9a799bf97dcb4
Ryan Young <youngrya001@gmail.com>
2023-01-14 16:41:57 +0000

    Update arch-desktop.sh

--- distros/arch-desktop.sh
+++ distros/arch-desktop.sh
@@ -6,17 +6,17 @@ sudo pacman -Syy
 
 sudo pacman -S thunderbird kmymoney steam discord flatpak libreoffice-still firefox netfetch nano lutris wine winetricks packagekit-qt5 fwupd nfs-utils -y
 
-flatpak install flathub com.github.iwalton3.jellyfin-media-player   #jellyfin player
-flatpak install flathub org.kde.kdenlive                            #kdenlive
-flatpak install flathub org.kde.krita                               #krita
-flatpak install flathub com.obsproject.Studio                       #obs-studio
-flatpak install flathub com.visualstudio.code                       #Visual Studio Code
-flatpak install flathub org.prismlauncher.PrismLauncher             #minecraft
-flatpak install flathub org.qbittorrent.qBittorrent                 #qbittorrent
-flatpak install flathub net.pcsx2.PCSX2                             #PCSX2
+flatpak install flathub com.github.iwalton3.jellyfin-media-player -y  #jellyfin player
+flatpak install flathub org.kde.kdenlive                          -y  #kdenlive
+flatpak install flathub org.kde.krita                             -y  #krita
+flatpak install flathub com.obsproject.Studio                     -y  #obs-studio
+flatpak install flathub com.visualstudio.code                     -y  #Visual Studio Code
+flatpak install flathub org.prismlauncher.PrismLauncher           -y  #minecraft
+flatpak install flathub org.qbittorrent.qBittorrent               -y  #qbittorrent
+flatpak install flathub net.pcsx2.PCSX2                           -y  #PCSX2
 
 touch /Desktop/update.sh
-sudo echo 'sudo pacman -Syu' >> /Desktop/update.sh
+sudo echo 'sudo pacman -Syu -y' >> /Desktop/update.sh
 sudo echo 'flatpak update -y' >> /Desktop/update.sh
 
 
