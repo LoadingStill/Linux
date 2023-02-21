173467381116261d4964ce29d9c9fceaebaa52d6
Ryan Young <youngrya001@gmail.com>
2023-02-21 14:21:03 -0500

    Update arch-desktop.sh

--- distros/arch-desktop.sh
+++ distros/arch-desktop.sh
@@ -8,15 +8,33 @@ sudo echo 'ParallelDownloads = 15' >> sudo /etc/pacman.conf
 
 sudo pacman -Syy
 
-sudo pacman -S thunderbird kmymoney steam discord flatpak libreoffice-still firefox netfetch nano lutris wine winetricks packagekit-qt5 fwupd nfs-utils git -y
+yes | sudo pacman -S thunderbird kmymoney steam discord flatpak libreoffice-still firefox neofetch nano lutris wine winetricks packagekit-qt5 fwupd nfs-utils git
+#thunderbird is an email and calender client
+#kmymoney is a budgeting tool
+#steam is for gaming
+#discord is for communication
+#flatpak is a different application package type that allows you to download and change packages on your system but the flatpak application is not effected because it has all dependencies in its own area
+#libreoffice-still is a office 265 replacement minus the cloud storage
+#firefox is your web-browser
+#neofetch is to show off your new arch install
+#nano is my perfered text edditor
+#lutris helps with running .exe applications
+#wine helps with running .exe applications
+#winetricks adds tools for wine
+#packageKit-qt5 is a system designed to make installing and updating software on your computer easier.
+#fwupd is an open-source daemon for managing the installation of firmware updates on Linux-based systems, developed by GNOME maintainer Richard Hughes.
+#nfs-utils lets you create nfs shares or mount a nfs share
+#git lets you close from git based webplatforms
 
-flatpak install flathub com.github.iwalton3.jellyfin-media-player -y  #jellyfin player
+
+
+flatpak install flathub com.github.iwalton3.jellyfin-media-player  -y  #jellyfin player
 #flatpak install flathub org.kde.kdenlive                          -y  #kdenlive
 #flatpak install flathub org.kde.krita                             -y  #krita
-flatpak install flathub com.obsproject.Studio                     -y  #obs-studio
-flatpak install flathub com.visualstudio.code                     -y  #Visual Studio Code
-flatpak install flathub org.prismlauncher.PrismLauncher           -y  #minecraft
-flatpak install flathub org.qbittorrent.qBittorrent               -y  #qbittorrent
+flatpak install flathub com.obsproject.Studio                      -y  #obs-studio
+flatpak install flathub com.visualstudio.code                      -y  #Visual Studio Code
+flatpak install flathub org.prismlauncher.PrismLauncher            -y  #minecraft
+flatpak install flathub org.qbittorrent.qBittorrent                -y  #qbittorrent
 #flatpak install flathub net.pcsx2.PCSX2                           -y  #PCSX2
 
 #touch /Desktop/update.sh
@@ -46,3 +64,14 @@ git clone https://aur.archlinux.org/1password.git
 cd 1password
 makepkg -si
 cd
+
+
+
+
+## yay
+yes | sudo pacman -S git
+cd
+mkdir AUR
+mkdir AUR/yay
+cd AUR/yay
+git clone https://aur.archlinux.org/yay.git
