2cc95a89a19270a3174f5df4daf2660733cbf1ad
Ryan Young <youngrya001@gmail.com>
2023-02-28 13:28:21 -0500

    Update install-aur-yay.sh

--- distros/software/pacman/install-aur-yay.sh
+++ distros/software/pacman/install-aur-yay.sh
@@ -5,3 +5,8 @@ pacman -S --needed git base-devel               # installs git and base-devel
 git clone https://aur.archlinux.org/yay.git     # clones the yay git hub
 cd yay                                          # change directory to yay
 makepkg -si                                     # make package -si
+
+
+
+
+# Source: https://github.com/Jguer/yay
