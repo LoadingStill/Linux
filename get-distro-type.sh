f5b6d34ab428865fc8961a5cd0d1d386a4a6f506
Ryan Young <youngrya001@gmail.com>
2023-03-15 18:27:27 -0400

    Update get-distro-type.sh

--- get-distro-type.sh
+++ get-distro-type.sh
@@ -24,8 +24,7 @@ echo "You are using $OS version $VER."
 
 
 #This script first checks for the presence of the /etc/os-release file, which is used by most modern Linux distributions to identify themselves.
-#If this file is present, it sources the file and extracts the NAME and VERSION_ID variables. If the /etc/os-release file is not found, the script 
-#checks for other common distribution-specific files like /etc/lsb-release, /etc/redhat-release, and /etc/gentoo-release.
+#If this file is present, it sources the file and extracts the NAME and VERSION_ID variables. If the /etc/os-release file is not found, the script checks for other common distribution-specific files like /etc/lsb-release, /etc/redhat-release, and /etc/gentoo-release.
 #If none of these files are found, the script falls back to using the uname command to retrieve the kernel name (usually "Linux") and kernel release version.
 #After determining the OS and version, the script prints out a message to the user.
 #Note that this script should work with any Linux distribution that provides one of the four files checked by the script.
