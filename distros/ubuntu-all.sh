0158a7d1c2c242d42fddd5c9e95c6337613d3389
Ryan Young <youngrya001@gmail.com>
2023-03-15 20:53:03 -0400

    Update ubuntu-all.sh

--- distros/ubuntu-all.sh
+++ distros/ubuntu-all.sh
@@ -45,29 +45,43 @@ echo "####################    Software Installed   #######################"
 echo "####################    Non-Free Software Selection   #######################"
 PS3='Please enter your choice: '
 options=("1 VS-Code" "2 Steam" "3 Discord" "4 Install ALL" "Quit")
-select opt in "${options[@]}"
-do
+
+install_vscode() {
+  sh software/apt/install-desktop-vscode.sh || echo "Failed to install VS-Code"
+}
+
+install_steam() {
+  sudo apt install -y steam || echo "Failed to install Steam"
+}
+
+install_discord() {
+  sh software/apt/install-desktop-discord.sh || echo "Failed to install Discord"
+}
+
+install_all() {
+  echo "This will install all applications. Are you sure? (y/n)"
+  read answer
+  if [ "$answer" == "y" ]; then
+    install_vscode
+    install_steam
+    install_discord
+  fi
+}
+
+while true; do
+  select opt in "${options[@]}"; do
     case $opt in
-        "VS-Code")
-            sh software/apt/install-desktop-vscode.sh #installs vs-code (owned by Microsoft)
-            ;;
-        "Steam")
-            sudo apt install -y steam #insatlls steam (owned by Valve)
-            ;;
-        "Discord")
-            sh software/apt/install-desktop-discord.sh #installs discord (owned by Discord)
-            ;;
-        "Install ALL")
-            sh software/apt/install-desktop-vscode.sh #installs vs-code (owned by Microsoft)
-            sh software/apt/install-desktop-steam.sh #insatlls steam (owned by Valve)
-            sh software/apt/install-desktop-discord.sh  #installs discord (owned by Discord)
-            ;;
-        "None")
-            break
-            ;;
-        *) echo "invalid option $REPLY";;
+      "1 VS-Code") install_vscode ;;
+      "2 Steam") install_steam ;;
+      "3 Discord") install_discord ;;
+      "4 Install ALL") install_all ;;
+      "Quit") exit ;;
+      *) echo "Invalid option $REPLY" ;;
     esac
+    break
+  done
 done
+
 echo "####################   Non-Free Software Installed    #######################"
 
 
