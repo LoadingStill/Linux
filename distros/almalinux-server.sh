36b01976fc41396cbe251089e325a65efc6a0a94
Ryan Young <youngrya001@gmail.com>
2023-10-13 14:09:10 -0400

    Update almalinux-server.sh

--- distros/almalinux-server.sh
+++ distros/almalinux-server.sh
@@ -1,1 +1,59 @@
+#!/bin/bash
 
+
+
+echo "#############################################################################"
+echo "#                                                                          #"
+echo "#                    AlmaLinux 8 Server Install Scrip                      #"
+echo "#                                                                          #"
+echo "#############################################################################"
+
+
+
+echo "####################    Updating    #######################"
+sudo dnf update -y
+echo "####################    Updated    #######################"
+
+
+
+echo "####################    Installing Software  #######################"
+
+echo "####################    Software Installed   #######################"
+
+
+
+echo "####################    Non-Free Software Installing   #######################"
+
+echo "####################   Non-Free Software Installed    #######################"
+
+
+
+echo "####################    After Software Updates    #######################"
+
+echo "####################    Completed Updates    #######################"
+
+
+
+echo "##########################################################################"
+echo "#                                                                        #"
+echo "#                    Completed almalinux-server.sh                       #"
+echo "#                                                                        #"
+echo "##########################################################################"
+
+
+
+echo "####################    Rebooting    #######################"
+echo "Rebooting in 5     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 4     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 3     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 2     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 1     Press Ctrl + C to cancel"
+sleep 1
+echo "Rebooting in 0     Press Ctrl + C to cancel"
+echo "####################    Rebooting    #######################"
+
+sudo reboot now
